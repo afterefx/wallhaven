99fa0681c7fc27508342aa5a439c22567ac689ea
Joshua McWilliams <jtmacnb@gmail.com>
2020-06-15 10:14:06 -0500

    initial

---
+++ app/src/main/java/com/mcwilliams/letscompose/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.mcwilliams.letscompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Modifier
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Icon
+import androidx.ui.foundation.Text
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Arrangement
+import androidx.ui.material.*
+import androidx.ui.res.imageResource
+import androidx.ui.res.vectorResource
+import androidx.ui.tooling.preview.Preview
+import com.mcwilliams.letscompose.ui.LetsComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LetsComposeTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Scaffold(
+        topAppBar = {
+            TopAppBar(
+                title = { Text(text = "Lets Compose") }
+//                navigationIcon = {
+//                    IconButton(onClick = { }) {
+//                        Icon(vectorResource(R.drawable.ic_input_add))
+//                    }
+//                }
+            )
+        },
+        bodyContent = {
+            Text(text = "Hello $name!")
+        },
+        bottomAppBar = {
+            BottomNavigation(
+                content = {
+                    listOf(
+                        BottomNavigationItem(
+                            selected = true, 
+                            text = { Text(text = "Home")},
+                            onSelected = {},
+                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_home_white_24dp))}
+                        ),
+                        BottomNavigationItem(
+                            selected = false,
+                            text = { Text(text = "Chat")},
+                            onSelected = {},
+                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_chat_white_24dp))}
+                        ),
+                        BottomNavigationItem(
+                            selected = false,
+                            text = { Text(text = "Profile")},
+                            onSelected = {},
+                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_account_circle_white_24dp))}
+                        )
+                    )
+                }
+            )
+        }
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LetsComposeTheme {
+        Greeting("Android")
+    }
+}

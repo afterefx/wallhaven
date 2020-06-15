5baa22fbafe9cb1badf4ff4e83d4fa78c774b820
Joshua McWilliams <jtmacnb@gmail.com>
2020-06-15 11:03:20 -0500

    setup home layout with a vertical scroller

--- app/src/main/java/com/mcwilliams/letscompose/MainActivity.kt
+++ app/src/main/java/com/mcwilliams/letscompose/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.shape.corner.CutCornerShape
 import androidx.ui.graphics.Color
-import androidx.ui.layout.Arrangement
+import androidx.ui.layout.*
 import androidx.ui.material.*
 import androidx.ui.res.imageResource
 import androidx.ui.res.vectorResource
+import androidx.ui.text.style.TextOverflow
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 import com.mcwilliams.letscompose.ui.LetsComposeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -40,35 +44,55 @@ fun Container() {
             )
         },
         bodyContent = {
-            Text(text = "Hello World")
+            homeBody()
         },
         bottomAppBar = {
             BottomNavigation(
                 content = {
                     listOf(
                         BottomNavigationItem(
-                            selected = true, 
-                            text = { Text(text = "Home")},
+                            selected = true,
+                            text = { Text(text = "Home") },
                             onSelected = {},
-                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_home_white_24dp))}
+                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_home_white_24dp)) }
                         ),
                         BottomNavigationItem(
                             selected = false,
-                            text = { Text(text = "Chat")},
+                            text = { Text(text = "Chat") },
                             onSelected = {},
-                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_chat_white_24dp))}
+                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_chat_white_24dp)) }
                         ),
                         BottomNavigationItem(
                             selected = false,
-                            text = { Text(text = "Profile")},
+                            text = { Text(text = "Profile") },
                             onSelected = {},
-                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_account_circle_white_24dp))}
+                            icon = { Icon(asset = vectorResource(id = R.drawable.ic_account_circle_white_24dp)) }
                         )
                     )
                 }
             )
         }
     )
+}
+
+@Composable
+fun homeBody() {
+    VerticalScroller {
+        for (x in 0..15) {
+            Row(modifier = Modifier.fillMaxWidth().absolutePadding(16.dp, 8.dp, 16.dp, 8.dp)) {
+                Card(modifier = Modifier.fillMaxWidth().height(60.dp),shape = MaterialTheme.shapes.medium, elevation = 4.dp) {
+                    Row(modifier = Modifier.padding(16.dp)) {
+                        Text(
+                            text = x.toString(),
+                            style = MaterialTheme.typography.h6,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 

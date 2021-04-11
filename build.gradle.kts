e49f3cded8fe5717586c862f2a5cc4626d8f4039
Christopher Carlisle <carlic578@gmail.com>
2021-04-10 23:20:38 -0500

    Update to AGP 7 alpha 14, kotlin 1.4.32 and gradle 7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 val composeVersion: String by extra { "1.0.0-beta02" }
-val kotlinVersion: String by extra { "1.4.31" }
+val kotlinVersion: String by extra { "1.4.32" }
 val hiltVersion: String by extra { "2.33-beta" }
 buildscript {
     repositories {
@@ -9,8 +9,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha10")
-        classpath(kotlin("gradle-plugin", version = "1.4.31"))
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
+        classpath(kotlin("gradle-plugin", version = "1.4.32"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
 
         // NOTE: Do not place your application dependencies here; they belong
